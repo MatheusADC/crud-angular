37faa8527238901dfe9bbbf8b4c03d7ea49b0707
MatheusSuper <matheusamaralsuper@gmail.com>
2025-02-18 10:25:40 -0300

    Add: Making an HTTP Get Request in Angular and RXJS

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
 
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+
 
 @Component({
   selector: 'app-courses',
@@ -11,7 +12,7 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   // coursesService: CoursesService;
