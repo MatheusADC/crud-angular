02fd9b6600bde4912bada9f33c72dc4528e00a0b
MatheusSuper <matheusamaralsuper@gmail.com>
2025-02-18 10:43:03 -0300

    Add: Implementation of the spinner for loading

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -12,7 +12,8 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Observable<Course[]>;
+  // courses: Course[] = [];
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   // coursesService: CoursesService;
@@ -20,7 +21,8 @@ export class CoursesComponent implements OnInit {
   constructor(private coursesService: CoursesService) { 
     // this.courses = [];
     // this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
+    // this.courses = this.coursesService.list().subscribe(courses => this.courses = courses);
   }
 
   ngOnInit(): void {
