fdc04dfa453bd4e394c5f91dd2b3865b2f4b8d40
MatheusSuper <matheusamaralsuper@gmail.com>
2025-02-19 13:49:30 -0300

    Add: Course Creation (HTTP POST)

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,7 +16,8 @@ export class CourseFormComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder, 
     private service: CoursesService,
-    private snackbar: MatSnackBar
+    private snackbar: MatSnackBar,
+    private location: Location
   ) { 
     this.form = this.formBuilder.group({
       name: [null],
@@ -29,11 +31,16 @@ export class CourseFormComponent implements OnInit {
   onSubmit() {
     // console.log(this.form.value);
     this.service.save(this.form.value)
-      .subscribe(result => console.log(result), error => this.onError());
+      .subscribe(result => this.onSuccess(), error => this.onError());
   }
 
   onCancel() {
+    this.location.back();
+  }
 
+  private onSuccess() {
+    this.snackbar.open('Curso salvo com sucesso!', '', { duration: 5000 });
+    this.onCancel();
   }
 
   private onError() {
